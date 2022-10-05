c7658c53340b4779a2404ae81f0bc38f440070ef
daninur <daninur84@gmail.com>
2022-10-05 23:14:32 +0700

    home-page

---
+++ src/components/Home/Social.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Social() {
+    return (
+        <div className="home_social">
+            <a href="https://www.instagram.com/daniinurrr/" className="home_social-icon" target="_blank">
+                <i class="uil uil-instagram"></i>
+            </a>
+            <a href="https://www.linkedin.com/in/dani-nur-adheanto-723053229/" className="home_social-icon" target="_blank">
+                <i class="uil uil-linkedin"></i>
+            </a>
+            <a href="https://github.com/DaniNurAdheanto" className="home_social-icon" target="_blank">
+                <i class="uil uil-github-alt"></i>
+            </a>
+        </div>
+    )
+}
+
+export default Social
